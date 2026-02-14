ce91f3759f34e16eb7be0044a4d08ac754717b5d
akankshathotwe07 <akankshathotwe21@gmail.com>
2026-02-14 13:23:52 +0530

    fixes some states issue

--- client/src/pages/admin/AddBlog.jsx
+++ client/src/pages/admin/AddBlog.jsx
@@ -61,6 +61,7 @@ const AddBlog = () => {
         toast.success(data.message);
         setImage(false);
         setTitle("");
+        setSubTitle("");
         quillRef.current.root.innerHTML = "";
         setCategory("Startup");
       } else {
